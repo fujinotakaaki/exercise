5120af85caa559546c2219b6eb89355a0b4c0e72
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-01-22 18:23:42 +0900

    add home controller, top page and /home/about

--- task3/config/routes.rb
+++ task3/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :books #, only: [:new, :create, :index, :show, :destroy]
-  #root設定まだ
+  get '/home/about' => 'homes#about'
+  root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
