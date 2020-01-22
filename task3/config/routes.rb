8f3cff507d7a40cdb48863275ee75668357c0a34
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-01-22 19:38:38 +0900

    add controller Users :show :index / create routes and view. and header and footer

--- task3/config/routes.rb
+++ task3/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books #, only: [:new, :create, :index, :show, :destroy]
+  resources :books
+  resources :users, only: [:show, :edit, :index]
   get '/home/about' => 'homes#about'
-  root 'homes#top'
+  root 'homes#top' #Sign_in / upはルートに飛ばされる
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
