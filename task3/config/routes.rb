e863c8f9caf15efcd2a24b42a8a7e55191719c15
fujinotakaaki <59040709+fujinotakaaki@users.noreply.github.com>
2020-01-28 13:54:25 +0900

    Revert "Task3"

--- task3/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :books, only: [:create, :destroy, :edit, :index, :show, :update]
-  resources :users,  only: [                            :edit, :index, :show, :update]
-  get '/home/about' => 'users#about'
-  root to: 'users#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
