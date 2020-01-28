3f06292542a51827298f6c94fa6ca194d46299fe
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-01-28 17:05:21 +0900

    [finish] 5-9 fourrh time. Rename task3 to task3-1. Performed $ bin/rails db:migrate:reset.

---
+++ task3-1/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '90599f58719e52e88a4fc435c4e336a207f7c6f9628de71fcda23e95df2af470b25b6db67794052e9eebc7bb00cdf23a010161086bce56fd9b9a37ef1635f002'
